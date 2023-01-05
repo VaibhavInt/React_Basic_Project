6369683e7be67ef7ec97a74c3146c0792f0df7cd
Vaibhav Shukla <vaibhavshukla2704@gmail.com>
2023-01-05 19:26:24 +0530

    project on birthday reminder completed

---
+++ birthday_reminder/src/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 1,
+    name: "MS Dhoni",
+    age: 41,
+    image:
+      "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRiII9371KsNrl7NJMJiH1MSBljoseqOwOyce6SHU1D63HY3ay0gowModGJ4DeZ6ZlORYbDeFMI7oKkQGA",
+  },
+  {
+    id: 2,
+    name: "Virat Kohli",
+    age: 34,
+    image:
+      "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/316600/316605.png",
+  },
+  {
+    id: 3,
+    name: "Virendra Sehwag",
+    age: 44,
+    image:
+      "https://st3.cricketcountry.com/wp-content/uploads/cricket/20150524070435.jpeg",
+  },
+  {
+    id: 4,
+    name: "Rohit Sharma",
+    age: 35,
+    image:
+      "https://static.tnn.in/photo/msid-96664966,imgsize-1832617,updatedat-1672580296822,width-200,height-200,resizemode-75/96664966.jpg",
+  },
+  {
+    id: 5,
+    name: "Gautam Gambhir",
+    age: 41,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Gautam_Gambhir_-_LS_MP.jpg/800px-Gautam_Gambhir_-_LS_MP.jpg",
+  },
+];
+
+export default data;
